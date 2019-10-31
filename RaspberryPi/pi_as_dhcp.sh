084b76fca455ecb2690830d43703916312badf2b
mhamilt <matt.hamilton@ed.ac.uk>
2019-10-31 20:31:43 +0000

    add static address notes

--- RaspberryPi/pi_as_dhcp.sh
+++ RaspberryPi/pi_as_dhcp.sh
@@ -32,9 +32,43 @@ sudo service dhcpcd restart
 #--------------------------------------------------------------
 sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
 sudo sh -c "printf 'interface=eth0      # Use interface eth0
+listen-address=%s      # Explicitly specify the address to listen on
 dhcp-range=%s,%s,24h                    # Assign IP addresses in range with a 12 hour lease
-' $DHCPRANGE $NETMASK  > /etc/dnsmasq.conf"
+' $ADDRESS $DHCPRANGE $NETMASK  > /etc/dnsmasq.conf"
 #--------------------------------------------------------------
 sudo systemctl reload dnsmasq
 #--------------------------------------------------------------
 #EOF
+
+# For configuring DHCP static addresses append this
+
+# # Static IPs
+# dhcp-host=00:00:5e:00:53:42,MyMac,192.168.0.10
+# dhcp-host=00:00:5e:00:53:01,00:00:5e:00:53:02,MyBridges,192.168.0.22
+# dhcp-host=00:00:5e:00:53:08,PC,192.168.0.23
+# dhcp-host=00:00:5e:00:53:10,KeTTLe,192.168.0.32
+
+# List
+# sudo iptables -t nat -v -L POSTROUTING -n --line-number
+# sudo iptables -t nat -v -L PREROUTING -n --line-number
+# sudo iptables -v -L FORWARD -n --line-number
+#
+# # Delete
+# sudo iptables -t nat -D POSTROUTING 1
+# sudo iptables -t nat -D PREROUTING 1
+# sudo iptables -D FORWARD 1
+# sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+# sudo iptables-restore < /etc/iptables.ipv4.nat
+# sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+#
+# # Add
+# sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j DNAT --to-destination 192.168.0.3
+# sudo iptables -A FORWARD -i wlan0 -o eth0 -p tcp --syn --dport 80 -m conntrack --ctstate NEW -j ACCEPT
+# sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 1234 -j DNAT --to-destination 192.168.0.3:80
+# sudo iptables -A FORWARD -i wlan0 -o eth0 -p tcp --syn --dport 1234 -m conntrack --ctstate NEW -j ACCEPT
+# sudo iptables -A FORWARD -i wlan0 -o eth0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+# sudo iptables -A FORWARD -i eth0 -o wlan0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+# sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+# sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+# cat /etc/iptables.ipv4.nat
+# sudo iptables-restore < /etc/iptables.ipv4.nat
