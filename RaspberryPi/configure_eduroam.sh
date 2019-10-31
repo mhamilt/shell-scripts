29f0ec1d1a01e454344946b92574db2b3bfd2b8c
mhamilt <matt.hamilton@ed.ac.uk>
2019-10-31 20:31:01 +0000

    add rasbpian buster workaround

--- RaspberryPi/configure_eduroam.sh
+++ RaspberryPi/configure_eduroam.sh
@@ -27,6 +27,16 @@ while [[ $INPUTMATCH -eq 0 ]]; do
 done
 printf '\n'
 #----------------------------------------------------------
+# temporary fix for WPA Enterprise on Raspbian Buster, connect Ethernet before running
+sudo apt-get remove wpasupplicant -y
+sudo mv -f /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo bash -c "echo 'deb http://raspbian.raspberrypi.org/raspbian/ stretch main contrib non-free rpi' > /etc/apt/sources.list"
+sudo apt-get update
+sudo apt-get install wpasupplicant -y
+sudo apt-mark hold wpasupplicant
+sudo cp -f /etc/apt/sources.list.bak /etc/apt/sources.list
+sudo apt-get update
+#----------------------------------------------------------
 sudo sh -c "printf 'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 ap_scan=1
 update_config=1
@@ -58,6 +68,25 @@ sudo systemctl restart dhcpcd
 #    eap=PEAP
 #    identity="UUN@DOMAIN"
 #    password="PASSWORD"
+#    phase1="peaplabel=0"
+#    phase2="auth=MSCHAPV2"
+# }
+
+
+# OR
+
+# echo -n plaintext_password_here | iconv -t utf16le | openssl md4
+# ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+# ap_scan=1
+# update_config=1
+# country=GB
+# network={
+#    ssid="eduroam"
+#    proto=RSN
+#    key_mgmt=WPA-EAP
+#    eap=PEAP
+#    identity="UUN@DOMAIN"
+#    password=hash:**************************************
 #    phase1="peaplabel=0"
 #    phase2="auth=MSCHAPV2"
 # }
